43cb9729f2696f033ffab6888601f86e06c04fad
Nicolas Darren Chetty <nicolas.chetty@chillisoft.co.za>
2019-01-08 16:56:08 +0200

    Working on second attempt of string kata

--- StringKata/StringKata.Tests/StringKataTests.cs
+++ StringKata/StringKata.Tests/StringKataTests.cs
@@ -6,6 +6,102 @@ namespace StringKata.Tests
     [TestFixture]
     public class StringKataTests
     {
+        private static StringKata CreateStringKata()
+        {
+            return new StringKata();
+        }
 
+        [Test]
+        public void Add_WhenEmptyString_ReturnsZero()
+        {
+            var input = "";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(0, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenOne_ReturnsOne()
+        {
+            var input = "1";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(1, actualResult);
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("2", 2)]
+        [TestCase("3", 3)]
+        [TestCase("4", 4)]
+        [TestCase("5", 5)]
+        public void Add_WhenOneDigit_ReturnsInput(string input, int expectedResult)
+        {
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenOneAndTwo_ReturnsThree()
+        {
+            var input = "1, 2";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(3, actualResult);
+        }
+
+        [TestCase("1, 2", 3)]
+        [TestCase("8,6", 14)]
+        [TestCase("1,2,3,4,5", 15)]
+        [TestCase("1,2,3,4,5,1,2,3,5", 26)]
+        public void Add_WhenTwoDigits_ReturnsSum(string input, int expectedResult)
+        {
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenNewLineDelimiter_ReturnsSum()
+        {
+            var input = "1\n2";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(3, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenNewLineDelimiterAndComma_ReturnsSum()
+        {
+            var input = "1\n2,3";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(6, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenCustomDelimiter_ReturnsSum()
+        {
+            var input = "//[*]\n1*2*3";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(6, actualResult);
+        }
+
+        [Test]
+        public void Add_WhenCustomDelimiterWithVaryingLength_ReturnsSum()
+        {
+            var input = "//[*|]\n1*|2*|3";
+
+            var actualResult = CreateStringKata().Add(input);
+
+            Assert.AreEqual(6, actualResult);
+        }
     }
 }
--- StringKata/StringKata/StringKata.cs
+++ StringKata/StringKata/StringKata.cs
@@ -9,7 +9,37 @@ namespace StringKata
     {
         public int Add(string input)
         {
-            return 0;
+            var sum = 0;
+
+            if (string.IsNullOrEmpty(input))
+                return sum;
+
+            char[] defaultDelimiter = {',', '\n'};
+            input = input.Trim();
+            List<int> numberList = new List<int>();
+
+            if (input.StartsWith("//"))
+            {
+                var endOfDelimiterSectionPosition = input.IndexOf('\n');
+                // Exclude the starting "//"
+                var delimiterSectionLength = endOfDelimiterSectionPosition - 2;
+                var delimiterSection = input.Substring(2, delimiterSectionLength).Replace("[", "").Replace(']',',');
+                var delimiters = delimiterSection.Split(defaultDelimiter, StringSplitOptions.RemoveEmptyEntries);
+
+                var numberString = input.Substring(endOfDelimiterSectionPosition + 1);
+                numberList = numberString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => Convert.ToInt32(x)).ToList();
+            }
+            else
+            {
+                numberList = input.Split(defaultDelimiter, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => Convert.ToInt32(x)).ToList();
+            }
+            
+            foreach (var number in numberList)
+                sum += number;
+
+            return sum;
         }
     }
 }
