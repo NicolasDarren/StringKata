af91f0f04a10d4b1c789c25cac1339ab6051993e
Nicolas Darren Chetty <nicolas.chetty@chillisoft.co.za>
2019-01-10 15:23:18 +0200

    Round 9 - Recorded

--- StringKata/StringKata.Tests/StringKataTests.cs
+++ StringKata/StringKata.Tests/StringKataTests.cs
@@ -15,100 +15,109 @@ namespace StringKata.Tests
         public void Add_WhenBlank_ReturnsZero()
         {
             var input = "";
+            var expectedResult = 0;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(0, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
-        public void Add_WhenOneValue_ReturnsValue()
+        public void Add_WhenOneValue_ReturnsSameValue()
         {
             var input = "1";
+            var expectedResult = 1;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(1, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenTwoValues_ReturnsSum()
         {
             var input = "1,2";
+            var expectedResult = 3;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(3, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenMultipleValues_ReturnsSum()
         {
             var input = "1,2,3,4,5";
+            var expectedResult = 15;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(15, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenNewLineDelimiter_ReturnsSum()
         {
-            var input = "1,2\n3,4,5";
+            var input = "1,2\n3";
+            var expectedResult = 6;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(15, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenCustomDelimiter_ReturnsSum()
         {
             var input = "//[*]\n1*2*3";
+            var expectedResult = 6;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(6, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
-        public void Add_WhenNegative_ThrowError()
+        public void Add_WhenNegativeValue_ThrowsException()
         {
-            var input = "1,-2,-3";
+            var input = "1,2,-3,-4";
 
             var exception = Assert.Throws<Exception>(() => GetStringKata().Add(input));
 
-            Assert.AreEqual(exception.Message, "Negative values not allowed. (-2,-3)");
+            Assert.AreEqual(exception.Message, "Negatives not allowed. (-3,-4)");
         }
 
         [Test]
-        public void Add_WhenValueGreaterThanThousand_IgnoresValueGreaterThanThousandAndReturnsSum()
+        public void Add_WhenValueGreaterThanAThousand_IgnoresValuesGreaterThanAThousandAndReturnsSum()
         {
-            var input = "1,2\n3,4,5000";
+            var input = "1,2,3000";
+            var expectedResult = 3;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(10, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenCustomDelimiterVaryingLength_ReturnsSum()
         {
             var input = "//[**]\n1**2**3";
+            var expectedResult = 6;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(6, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
         public void Add_WhenMultipleCustomDelimiterVaryingLength_ReturnsSum()
         {
             var input = "//[**][||]\n1||2**3";
+            var expectedResult = 6;
 
             var actualResult = GetStringKata().Add(input);
 
-            Assert.AreEqual(6, actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
--- StringKata/StringKata/StringKata.cs
+++ StringKata/StringKata/StringKata.cs
@@ -12,10 +12,10 @@ namespace StringKata
             if (string.IsNullOrWhiteSpace(input))
                 return 0;
 
-            var delimiters = GetDelimiter(input);
+            var delimiters = GetDelimiters(input);
 
             var numberList = GetNumberList(input, delimiters);
-                
+
             var sum = numberList.Sum();
 
             return sum;
@@ -28,33 +28,31 @@ namespace StringKata
             if (HasCustomDelimiter(input))
                 numberString = input.Split('\n')[1];
 
-            var numberList = numberString
-                .Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
-                .ToList();
+            var numberList = numberString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
 
-            ThrowErrorIfAnyNegativeValues(numberList);
+            ThrowExceptionIfAnyNegativeValues(numberList);
 
-            numberList = RemoveValuesGreaterThanThousand(numberList);
-
+            numberList = RemoveValuesGreaterThanAThousand(numberList);
             return numberList;
         }
 
-        private List<int> RemoveValuesGreaterThanThousand(List<int> numberList)
+        private List<int> RemoveValuesGreaterThanAThousand(List<int> numberList)
         {
             return numberList.Where(x => x < 1000).ToList();
         }
 
-        private void ThrowErrorIfAnyNegativeValues(List<int> numberList)
+        private void ThrowExceptionIfAnyNegativeValues(List<int> numberList)
         {
             var negativeValues = numberList.Where(x => x < 0).ToList();
 
-            if (negativeValues.Count > 0)
-                throw new Exception($"Negative values not allowed. ({string.Join(",", negativeValues)})");
+            if(negativeValues.Count > 0)
+                throw new Exception($"Negatives not allowed. ({string.Join(",", negativeValues)})");
         }
 
-        private string[] GetDelimiter(string input)
+        private string[] GetDelimiters(string input)
         {
             var delimiters = new[] {",", "\n"};
+
             if (HasCustomDelimiter(input))
                 delimiters = GetCustomDelimiters(input);
 
@@ -63,12 +61,12 @@ namespace StringKata
 
         private string[] GetCustomDelimiters(string input)
         {
-            var delimiterString = input.Split('\n').FirstOrDefault();
-            var customDelimiterIndicatorLength = 2;
-            var commaSeparatedDelimiters = delimiterString.Substring(customDelimiterIndicatorLength)
-                .Replace("[", "");
+            var delimiterString = input.Split('\n')[0]
+                .Remove(0,2);
+            var delimiters = delimiterString.Replace("[", "")
+                .Split(']');
 
-            return commaSeparatedDelimiters.Split(']');
+            return delimiters;
         }
 
         private bool HasCustomDelimiter(string input)
