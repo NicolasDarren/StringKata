d46601f18baca7621e57e320e5aecfcc9471e5ba
Nicolas Darren Chetty <nicolas.chetty@chillisoft.co.za>
2019-03-13 08:18:57 +0200

    Complete

--- StringKata/StringKata.Tests/StringKataTests.cs
+++ StringKata/StringKata.Tests/StringKataTests.cs
@@ -72,6 +72,50 @@ namespace StringKata.Tests
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        [Test]
+        public void Add_NegativeNumberPassed_ThrowsError()
+        {
+            var input = "1,2,-3,-4";
+            var expectedMessage = "Negatives are not allowed: -3,-4";
+
+            var exception = Assert.Throws<Exception>(() => GetStringKata().Add(input));
+
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void Add_NumberGreaterThanThousandPassed_NumberGreaterThanThousandIgnoredAndReturnsSum()
+        {
+            var input = "1,2,3,4000";
+            var expectedResult = 6;
+
+            var actualResult = GetStringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_CustomDelimiterVaryingLength_ReturnsSum()
+        {
+            var input = "//***\n1***2***3";
+            var expectedResult = 6;
+
+            var actualResult = GetStringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_MultipleCustomDelimitersVaryingLength_ReturnsSum()
+        {
+            var input = "//[***][||]\n1||2***3";
+            var expectedResult = 6;
+
+            var actualResult = GetStringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
         private StringKata GetStringKata()
         {
             return new StringKata();
--- StringKata/StringKata/StringKata.cs
+++ StringKata/StringKata/StringKata.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
 
 namespace StringKata
 {
@@ -12,23 +13,43 @@ namespace StringKata
             if (string.IsNullOrWhiteSpace(input))
                 return 0;
 
-            var delimiters = new char[] {',', '\n'};
+            var delimiters = new[] {','.ToString(), '\n'.ToString()};
+            var numberString = input;
 
             if (input.StartsWith("//"))
+            {
                 delimiters = GetCustomDelimiter(input);
+                numberString = input.Split('\n')[1];
+            }
 
-            var numberString = input.Split(delimiters);
-
-            var numberList = numberString.Select(int.Parse).ToList();
+            var numberList = GetNumberList(numberString, delimiters);
 
             return numberList.Sum();
         }
 
-        private char[] GetCustomDelimiter(string input)
+        private static List<int> GetNumberList(string numberString, string[] delimiters)
+        {
+            var numberList = numberString
+                .Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(number => number < 1000)
+                .ToList();
+            var negativeNumbers = numberList.Where(number => number < 0).ToList();
+
+            if(negativeNumbers.Any())
+                throw new Exception("Negatives are not allowed: " + string.Join(",", negativeNumbers));
+
+            return numberList;
+        }
+
+        private string[] GetCustomDelimiter(string input)
         {
             var delimiterSection = input.Split('\n')[0].Substring(2);
-
-            return delimiterSection.ToArray();
+            var delimiterArray = delimiterSection
+                .Replace("[", "")
+                .Replace(']', ',')
+                .Split(',');
+            return delimiterArray;
         }
     }
 }
