45f2c76839216f851edab05b8755fc70f3dcacb5
Nicolas Darren Chetty <nicolas.chetty@chillisoft.co.za>
2019-07-03 08:01:30 +0200

    Reset

--- StringKata/StringKata/StringKata.cs
+++ StringKata/StringKata/StringKata.cs
@@ -7,23 +7,7 @@ namespace StringKata
     {
         public int Add(string numbersString)
         {
-            if (string.IsNullOrWhiteSpace(numbersString))
-            {
-                return 0;
-            }
-            var delimiters = new [] { ',', '\n' };
-            if (numbersString.StartsWith("//"))
-            {
-                delimiters = numbersString.ToCharArray(2,1);
-                var firstNewLine = numbersString.IndexOf('\n');
-                numbersString = numbersString.Substring(firstNewLine + 1);
-            }
-
-            var numberList = numbersString.Split(delimiters).Select(int.Parse);
-
-            var result = numberList.Sum();
-
-            return result;
+            throw new NotImplementedException();
         }
     }
 }
