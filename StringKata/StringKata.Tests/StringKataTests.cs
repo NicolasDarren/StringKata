f2aaf415c25683bccdd3174b7e1687b37e45d023
Nicolas Darren Chetty <nicolas.chetty@chillisoft.co.za>
2019-08-05 09:15:38 +0200

    Morning attempt

--- StringKata/StringKata.Tests/StringKataTests.cs
+++ StringKata/StringKata.Tests/StringKataTests.cs
@@ -1,10 +1,116 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace StringKata.Tests
 {
     [TestFixture]
     public class StringKataTests
     {
+        [Test]
+        public void Add_GivenEmptyString_ShouldReturnZero()
+        {
+            var input = "";
+            var expectedResult = 0;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_GivenOneInput_ShouldReturnSameNumber()
+        {
+            var input = "2";
+            var expectedResult = 2;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_GivenTwoInputs_ShouldReturnSum()
+        {
+            var input = "2,3";
+            var expectedResult = 5;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase("2,3,4", 9)]
+        [TestCase("1,2,3,4,5,6,7,8,9,10", 55)]
+        public void Add_GivenMultipleInputs_ShouldReturnSum(string input, int expectedResult)
+        {
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
         
+        [Test]
+        public void Add_UsingNewLineDelimiter_ShouldReturnSum()
+        {
+            var input = "2,3\n4";
+            var expectedResult = 9;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_UsingCustomDelimiter_ShouldReturnSum()
+        {
+            var input = "//;\n2;3;4";
+            var expectedResult = 9;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumber_ShouldThrowError()
+        {
+            var input = "2,3,-4";
+
+            Assert.Throws<Exception>(() => StringKata().Add(input), "Negatives are not allowed: -4");
+        }
+
+        [Test]
+        public void Add_GivenNegativeNumbers_ShouldThrowError()
+        {
+            var input = "2,3,-4,-5";
+
+            Assert.Throws<Exception>(() => StringKata().Add(input), "Negatives are not allowed: -4, -5");
+        }
+
+        [Test]
+        public void Add_GivenNumbersGreaterThanOneThousand_ShouldIgnoreNumbersGreaterThanOneThousandAndReturnSum()
+        {
+            var input = "2,3,1001";
+            var expectedResult = 5;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Add_CustomDelimiterCanBeAStringOfAnyLength_ShouldReturnSum()
+        {
+            var input = "//[***]\n1***2***3";
+            var expectedResult = 6;
+
+            var actualResult = StringKata().Add(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        private static StringKata StringKata()
+        {
+            return new StringKata();
+        }
     }
 }
--- StringKata/StringKata/StringKata.cs
+++ StringKata/StringKata/StringKata.cs
@@ -1,13 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework;
 
 namespace StringKata
 {
     public class StringKata
     {
-        public int Add(string numbersString)
+        public int Add(string input)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            var delimiters = new[] { ",", "\n"};
+            var numberString = input;
+
+            if (input.StartsWith("//"))
+            {
+                delimiters = GetCustomDelimiter(input, delimiters);
+                numberString = GetNumberString(input);
+            }
+
+            var numberList = GetNumberList(numberString, delimiters);
+
+            numberList = RemoveValuesGreaterThanOneThousand(numberList);
+
+            CheckForNegatives(numberList);
+
+            return numberList.Sum();
+        }
+
+        private static List<int> RemoveValuesGreaterThanOneThousand(List<int> numberList)
+        {
+            numberList = numberList.Where(x => x < 1000).ToList();
+            return numberList;
+        }
+
+        private static void CheckForNegatives(List<int> numberList)
+        {
+            var negatives = numberList.Where(x => x < 0).ToList();
+
+            if (negatives.Any())
+                throw new Exception("Negatives are not allowed: " + string.Join(", ", negatives));
+        }
+
+        private static List<int> GetNumberList(string numberString, string[] delimiters)
+        {
+            return numberString.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        }
+
+        private static string GetNumberString(string input)
+        {
+            var newLineIndex = input.IndexOf('\n');
+            var numberString = input.Substring(newLineIndex + 1);
+            return numberString;
+        }
+
+        private static string[] GetCustomDelimiter(string input, string[] delimiters)
+        {
+            var newLineIndex = input.IndexOf('\n');
+            var delimiterString = input.Substring(2, newLineIndex - 2);
+
+            if (delimiterString.Contains('['))
+            {
+                var startIndex = delimiterString.IndexOf('[');
+                if (startIndex > -1)
+                    delimiterString = delimiterString.Remove(startIndex, 1);
+
+                var endIndex = delimiterString.IndexOf(']');
+                if(endIndex > -1)
+                    delimiterString = delimiterString.Remove(endIndex,1);
+
+                return new[] { delimiterString };
+            }
+
+            var del = input.Substring(2, 1);
+            delimiters = delimiters.Append(del).ToArray();
+            return delimiters;
         }
     }
 }
